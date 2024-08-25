daa76b9659b1224f99ceab819b3a5c352e69dcb8
IronEddiee <leguizamonmauroezequiel@gmail.com>
2024-08-25 00:00:15 -0300

    nuevas actividades

---
+++ Actividad5.java
@@ -0,0 +1,50 @@
+public class Actividad5 {
+   public static void main (String[] args){
+     char dia;
+     System.out.println("Que dia es hoy?");
+     dia = Console.readChar();
+     switch (dia){
+      case 'l':
+         {    
+         System.out.println("Hoy no hay clases de Programaion de Algoritmos");
+         break;
+         }
+         case 'm':
+         {    
+         System.out.println("HOY TENGO CLASES DE PROGRAMACION DE ALGORITMOS");
+         break;
+         }
+         case 'i':
+         {    
+         System.out.println("HOY TENGO CLASES DE PROGRAMACION DE ALGORITMOS");
+         break;
+         }
+         case 'j':
+         {    
+         System.out.println("HOY TENGO CLASES DE PROGRAMACION DE ALGORITMOS");
+         break;
+         }
+         case 'v':
+         {    
+         System.out.println("Hoy no hay clases de Programaion de Algoritmos");
+         break;
+         }
+         case 's':
+         {    
+         System.out.println("Hoy no hay clases de Programaion de Algoritmos");
+         break;
+         }
+         case 'd':
+         {    
+         System.out.println("Hoy no hay clases de Programaion de Algoritmos");
+         break;
+         }
+          default:
+         {    
+         System.out.println("La letra ingresada no es valida");
+         break;
+         }
+      }
+     
+   }
+}

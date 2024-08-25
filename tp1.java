0627bb64d4d61f66495b63c3ad7cc276a193327e
IronEddiee <leguizamonmauroezequiel@gmail.com>
2024-08-24 23:32:21 -0300

    actualizacion

--- tp1.java
+++ tp1.java
@@ -1,5 +1,21 @@
 public class tp1 {
    public static void main (String[] args){
-     System.out.println("hola");
+     //actividad 4
+     int nro1, nro2, nro3;
+     System.out.println("Ingrese 3 numeros enteros");
+     nro1 = Console.readInt();     
+     nro2 = Console.readInt(); 
+     nro3 = Console.readInt(); 
+     if (nro1 > nro2 & nro1 > nro3){
+      System.out.println("El numero mayor es: " + nro1);
+      }
+     else{
+      if (nro2 > nro1 & nro2 > nro3){
+         System.out.println("El numero mayor es: " + nro2);
+         }
+      else{
+         System.out.println("El numero mayor es: " + nro3);
+         }
+      }
    }
 }
